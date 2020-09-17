bc5856d8aa63271f03895c16737fda7807527002
Rodrigo Avellar <roavellarm@gmail.com>
2020-09-17 03:14:11 -0300

    Update Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 import abc from '../../assets/images/abc-button.png'
 import book from '../../assets/images/book-button.png'
 import pencil from '../../assets/images/pencil-button.png'
@@ -6,9 +7,11 @@ import Button from '../../components/HomeButton'
 import { Container } from './styles'
 
 export default function Home() {
+  const { navigate } = useNavigation()
+
   return (
     <Container>
-      <Button image={abc} />
+      <Button image={abc} onPress={() => navigate('LettersOptions')} />
       <Button image={book} />
       <Button image={pencil} />
     </Container>
