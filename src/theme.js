ef33d7d2df75b58510a82147d9073e5acda2f51c
Rodrigo Avellar <roavellarm@gmail.com>
2020-09-17 01:52:13 -0300

    :lipstick: Add theme for ThemeProvider

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+const text = {
+  fontFamily: 'Jua_400Regular',
+}
+
+const colors = {
+  primary: '',
+  secondary: '',
+  background: '#4CB3FF',
+  buttonBackground: '#2F5AD8',
+}
+
+export default {
+  text,
+  colors,
+}
