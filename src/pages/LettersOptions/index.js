36269c957d1e48a6f7275de688a3249dfcf1f3b7
Rodrigo Avellar <roavellarm@gmail.com>
2020-09-17 03:56:01 -0300

    Update LettersOptions

--- src/pages/LettersOptions/index.js
+++ src/pages/LettersOptions/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 import faderImg from '../../assets/images/fader.png'
 import * as S from './styles'
 
 export default function LettersOptions() {
   const options = [5, 4, 3, 2, 1]
+  const { navigate } = useNavigation()
+
+  const handleChoice = (choice) =>
+    navigate({ name: 'Letter', params: { choice } })
 
   return (
     <S.Container>
@@ -12,17 +17,13 @@ export default function LettersOptions() {
         letras!
       </S.Text>
 
-      {/* <S.ButtonsContainer> */}
       <S.ImageBackground source={faderImg} resizeMode="contain">
-        {options.map((option) => {
-          return (
-            <S.Button key={option}>
-              <S.ButtonText>{option}</S.ButtonText>
-            </S.Button>
-          )
-        })}
+        {options.map((option) => (
+          <S.Button key={option} onPress={() => handleChoice(option)}>
+            <S.ButtonText>{option}</S.ButtonText>
+          </S.Button>
+        ))}
       </S.ImageBackground>
-      {/* </S.ButtonsContainer> */}
     </S.Container>
   )
 }
