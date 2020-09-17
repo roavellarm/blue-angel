7a31929e9f5b4bb12d72c80f5308c07c6aaf5273
Rodrigo Avellar <roavellarm@gmail.com>
2020-09-17 01:49:45 -0300

    :rocket: Start project

---
+++ App.js
@@ -0,0 +1,24 @@
+import 'react-native-gesture-handler'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { AppLoading } from 'expo'
+import { NavigationContainer } from '@react-navigation/native'
+import { Jua_400Regular, useFonts } from '@expo-google-fonts/jua'
+import { ThemeProvider } from 'styled-components/native'
+import theme from './src/theme'
+import Router from './src/Router'
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Jua_400Regular })
+
+  if (!fontsLoaded) return <AppLoading />
+
+  return (
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <Router />
+      </NavigationContainer>
+      <StatusBar style="light" />
+    </ThemeProvider>
+  )
+}
