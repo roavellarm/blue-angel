fc0258d0f6f3377e78dcf4838a48f5e7b939b581
Rodrigo Avellar <roavellarm@gmail.com>
2020-09-26 18:02:35 -0300

    Add Container Component

---
+++ src/components/Container/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+const StyledContainer = styled.ScrollView`
+  flex: 1;
+  background: ${({ color }) => color || '#4cb3ff'};
+  padding: ${({ hasPadding }) => (hasPadding ? '30px 12px' : '30px 0px')};
+`
+
+export default function Container({ color, hasPadding = true, children }) {
+  return (
+    <StyledContainer
+      color={color}
+      hasPadding={hasPadding}
+      contentContainerStyle={{ alignItems: 'center', justifyContent: 'center' }}
+    >
+      {children}
+    </StyledContainer>
+  )
+}
