d387603a562cc6b1cd8fe33474be4f0c26bf562f
Rodrigo Avellar <roavellarm@gmail.com>
2020-11-10 17:51:18 -0300

    Add linter to scripts

--- src/pages/Syllable/Page4/index.js
+++ src/pages/Syllable/Page4/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Feather } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
 import Container from '../../../components/Container'
