892c94f6c0792011d736543bb59997fdd44e518a
Rodrigo Avellar <roavellarm@gmail.com>
2020-11-10 17:44:18 -0300

    Add delayTime and timeout to utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,3 +5,10 @@ export const checkLetterSpell = (letter) => {
   if (letter === 'O') return 'Ó'
   return letter
 }
+
+const timeout = (delay) => new Promise((res) => setTimeout(res, delay))
+
+export const delayTime = async (delay = 3000) => {
+  await timeout(delay)
+  return null
+}
