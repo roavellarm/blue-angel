e5be1fab9337140b837d1c95b717a847b4a1543c
Rodrigo Avellar <roavellarm@gmail.com>
2020-11-27 12:23:20 -0300

    Fix two words

--- src/pages/Phrase/Page4/exercicies/twoWords.js
+++ src/pages/Phrase/Page4/exercicies/twoWords.js
@@ -10,39 +10,33 @@ import {
 const twoWords = [
   {
     image: girafa,
-    correctAnswer: ['Girafa', 'Amarela'],
+    correctAnswer: 'Girafa Amarela',
     options: ['Girafa', 'Verde', 'Amarela'],
-    word: 'Girafa',
   },
   {
     image: ketchup,
-    correctAnswer: ['Ketchup', 'Vermelho'],
+    correctAnswer: 'Ketchup Vermelho',
     options: ['Ketchup', 'Redondo', 'Vermelho'],
-    word: 'Ketchup',
   },
   {
     image: laço,
-    correctAnswer: ['Laço', 'Rosa'],
+    correctAnswer: 'Laço Rosa',
     options: ['Laço', 'Rosa', 'Preto'],
-    word: 'Laço',
   },
   {
     image: queijo,
-    correctAnswer: ['Queijo', 'Amarelo'],
+    correctAnswer: 'Queijo Amarelo',
     options: ['Queijo', 'Verde', 'Amarela'],
-    word: 'Queijo',
   },
   {
     image: sapo,
-    correctAnswer: ['Sapo', 'Verde'],
+    correctAnswer: 'Sapo Verde',
     options: ['Sapo', 'Verde', 'Colorido'],
-    word: 'Girafa',
   },
   {
     image: uva,
-    correctAnswer: ['Uva', 'Roxa'],
+    correctAnswer: 'Uva Roxa',
     options: ['Uva', 'Verde', 'Roxa'],
-    word: 'Uva',
   },
 ]
 
