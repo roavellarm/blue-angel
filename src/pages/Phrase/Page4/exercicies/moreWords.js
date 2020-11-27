2cce7496c6de063ddf82dd537d2a817441adc6e4
Rodrigo Avellar <roavellarm@gmail.com>
2020-11-27 12:22:55 -0300

    Fix moreWords ex

--- src/pages/Phrase/Page4/exercicies/moreWords.js
+++ src/pages/Phrase/Page4/exercicies/moreWords.js
@@ -8,27 +8,23 @@ import {
 const moreWords = [
   {
     image: bicycle,
-    correctAnswer: ['Ele', 'está', 'andando', 'de', 'bicicleta'],
+    correctAnswer: 'Ele está andando de bicicleta',
     options: ['Ele', 'está', 'andando', 'de', 'bicicleta', 'gira'],
-    word: 'bicycle',
   },
   {
     image: washplates,
-    correctAnswer: ['Ela', 'está', 'lavando', 'a', 'louça'],
+    correctAnswer: 'Ela está lavando a louça',
     options: ['Ela', 'está', 'lavando', 'a', 'louça', 'gira'],
-    word: 'washplates',
   },
   {
     image: fixfather,
-    correctAnswer: ['Ela', 'arruma', 'a', 'gravata'],
+    correctAnswer: 'Ela arruma a gravata',
     options: ['Ela', 'arruma', 'a', 'gravata', 'gira'],
-    word: 'fixfather',
   },
   {
     image: handwash,
-    correctAnswer: ['Está', 'lavando', 'suas', 'mãos'],
+    correctAnswer: 'Está lavando suas mãos',
     options: ['Está', 'lavando', 'suas', 'mãos', 'gira'],
-    word: 'handwash',
   },
 ]
 
